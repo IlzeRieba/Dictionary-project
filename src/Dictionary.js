9813a80a49901abe562fe0a2eb48e61e32e86907
IlzeRieba <78449581+IlzeRieba@users.noreply.github.com>
2022-07-03 14:26:39 +0200

    created photo component and integrated Pexels api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 
 export default function Dictionary(props) 
@@ -9,6 +10,7 @@ export default function Dictionary(props)
 let [ keyWord, setKeyword] = useState(props.defaultKeyword);
 let [results, setResults] = useState(null);
 let [loaded, setLoaded] = useState(false);
+let [photos, setPhotos] = useState(null);
 
 function handleResponse(response) {
 
@@ -16,10 +18,21 @@ setResults(response.data[0]);
 
 }
 
+function handlePexelsResponse(response) {
+    console.log(response.data);
+    setPhotos(response.data.photos);
+}
+
 function search() {
   //documentation https://dictionaryapi.dev/
   let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
   axios.get(apiUrl).then(handleResponse);
+
+  let pexelsApiKey = "563492ad6f91700001000001caf5510f27284d9eb47f370ff594ab93";
+let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyWord}&per_page=9`;
+let headers = { Authorization : `Bearer ${pexelsApiKey}`};
+axios.get(pexelsApiUrl, { headers: headers,}).then(handlePexelsResponse);
+
 }
 
     function handleSubmit(event) {
@@ -53,6 +66,7 @@ if (loaded) {
       </section>
 
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 } else {
